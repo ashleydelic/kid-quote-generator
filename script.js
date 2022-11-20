28cc6c40508d7bf881f06bc2ca5ae1916c7af9c7
ashleydelic <ashleycohzer96@gmail.com>
2022-11-19 20:28:30 -0500

    quotes added

---
+++ script.js
@@ -0,0 +1,50 @@
+const quotes = [{
+    quote: `“Never follow anyone else's path. Unless you're in the woods and you're lost and you see a path. Then by all means follow that path.”`,
+    author: '- Ellen DeGeneres.'
+},
+{
+    quote: `“The odds of going to the store for a loaf of bread and coming out with only a loaf of bread are three billion to one.”`,
+    author: '- Erma Bombeck.'
+},
+{
+    quote: `“Have you any idea how many children it takes to turn off one light in the kitchen? Three. It takes one to say,  'What light?' and two more to say, 'I didn't turn it on' .”`,
+    author: `- Erma Bombeck.`
+},
+{
+    quote: `“There's nothing sadder in this world than to awake Christmas morning and not be a child.”`,
+    author: `- Erma Bombeck.`
+},
+{
+    quote: `“When your mother asks, 'Do you want a piece of advice?' it's a mere formality. It doesn't matter if you answer yes or no. You're going to get it anyway.”`,
+    author: `- Erma Bombeck.`
+},
+{
+    quote: `“The more you give away the more happy you become. Why fit in when you were born to stand out?”`,
+    author: ` Dr. Suess.`
+},
+{
+    quote: `“No one is perfect, that's why pencils have erasers.”`,
+    author: `- Wolfgang Riebe.`
+},
+{
+    quote: `“We must be the change we want to see.”`,
+    author: `- Mahatma Gandhi.`
+},
+{
+    quote: `“Who says that my dreams have to stay just my dreams?”`,
+    author: `- Ariel, 'The Little Mermaid.'`
+},
+{
+    quote: `“If you can't say anything nice, don't say anything at all.”`,
+    author: `- Thumper, 'Bambi.'`
+},
+{
+    quote: `“Forgiveness does not change the past, but it does enlarge the future.”`,
+    author: `- Paul Boese.`
+},
+{
+    quote: `“Yesterday is history. Tomorrow is a mystery. Today is a gift. That's why we call it 'The Present.”`,
+    author: `- Eleanor Roosevelt.`
+}
+]
+
